fbec9a8fd5d17bfc0f8d77d3df4f119d1218732d
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-25 11:32:22 +0200

    add array of bicycle

---
+++ main.js
@@ -0,0 +1,22 @@
+const bicycle = [
+  {
+    brand: 'Campagnolo',
+    peso: '11kg'
+  },
+  {
+    brand: 'Deda Elementi',
+    peso: '10kg'
+  },
+  {
+    brand: 'Fizik',
+    peso: '12kg'
+  },
+  {
+    brand: 'Civic',
+    peso: '1010kg'
+  }
+]
+
+const container = document.querySelector('.container')
+const card = document.querySelector('.card')
+
