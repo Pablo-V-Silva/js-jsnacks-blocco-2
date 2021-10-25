47975c5e2282516e69fb1a839da1d6152ca2bb79
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-25 12:11:52 +0200

    correct version

--- main.js
+++ main.js
@@ -13,13 +13,39 @@ const bicycle = [
   },
   {
     brand: 'Civic',
-    peso: 101
+    peso: 3
   }
 ]
 
-const container = document.querySelector('.container');
-const card = document.querySelector('.card');
+/*
+// const minWeight = Math.min(bicycle.peso);
 
-const minWeight = Math.min(bicycle.peso);
+// console.log(minWeight);
 
-console.log(minWeight);
+for (let i = 0; i < bicycle.length; i++) {
+  const element = bicycle[i];
+  console.log(element);
+  for (const key in bicycle) {
+    console.log(Math.min(bicycle.peso))
+  }
+
+} */
+
+// prendere una bici di riferimento
+let lightestBike = bicycle[0]
+//ciclare fra le bici 
+for (let i = 0; i < bicycle.length; i++) {
+  const bike = bicycle[i];
+  console.log(bike.peso);
+
+  if (bike.peso < lightestBike.peso) {
+    lightestBike = bike;
+  }
+
+}
+
+console.log(lightestBike);
+// verificare se la bici corrente è più leggere di quella selezionata
+
+document.querySelector('.card').innerHTML = `
+la bici più leggera è ${lightestBike.brand} e pesa ${lightestBike.peso}kg`;
