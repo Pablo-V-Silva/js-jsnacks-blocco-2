36e7d8a222700d7d35ac4cb9adf9c11395da6cda
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-25 12:19:35 +0200

    Array of soccerSquads

---
+++ snack2/main.js
@@ -0,0 +1,22 @@
+const soccerSquads = [
+  {
+    name: 'Milan',
+    points: 0,
+    falls: 0
+  },
+  {
+    name: 'Inter',
+    points: 0,
+    falls: 0
+  },
+  {
+    name: 'Juve',
+    points: 0,
+    falls: 0
+  },
+  {
+    name: 'Real Madrid',
+    points: 0,
+    falls: 0
+  }
+]
