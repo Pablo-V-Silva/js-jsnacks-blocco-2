aefb127925b87853dd479caa95deb31fce8b523c
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-25 11:50:09 +0200

    NaN on minWeight

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 const bicycle = [
   {
     brand: 'Campagnolo',
-    peso: '11kg'
+    peso: 11
   },
   {
     brand: 'Deda Elementi',
-    peso: '10kg'
+    peso: 10
   },
   {
     brand: 'Fizik',
-    peso: '12kg'
+    peso: 12
   },
   {
     brand: 'Civic',
-    peso: '1010kg'
+    peso: 101
   }
 ]
 
-const container = document.querySelector('.container')
-const card = document.querySelector('.card')
+const container = document.querySelector('.container');
+const card = document.querySelector('.card');
 
+const minWeight = Math.min(bicycle.peso);
+
+console.log(minWeight);
